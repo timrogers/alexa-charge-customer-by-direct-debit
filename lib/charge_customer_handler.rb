57696d2d5f4846861ed7f2f9ad7d3c948ec85dc6
Tim Rogers <tim@gocardless.com>
2016-11-15 19:58:43 +0200

    Lots of refactoring

--- lib/charge_customer_handler.rb
+++ lib/charge_customer_handler.rb
@@ -4,28 +4,34 @@ require_relative 'customer_lookup_service'
 require_relative 'payment_service'
 
 class ChargeCustomerHandler < AlexaSkillsRuby::Handler
-  on_intent('ChargeCustomer') do
-    given_name = request.intent.slots['GivenName']
-    amount = request.intent.slots['Amount'].to_i
-
-    charge_customer(given_name: given_name, amount: amount)
-  end
+  on_intent('ChargeCustomer') { charge_customer }
 
   private
 
-  def charge_customer(given_name:, amount:)
+  def charge_customer
     customer = customers.find_by_given_name(given_name)
+    return customer_not_found_message if customer.nil?
 
-    return customer_not_found_message(given_name) if customer.nil?
-
-    payment = payments.charge(customer: customer, amount_in_pence: amount * 100)
-    charged_successfully_message(given_name, amount, payment.charge_date)
+    payment = payments.charge(customer: customer, amount_in_pence: amount_in_pence)
+    charged_successfully_message(payment.charge_date)
   rescue CustomerLookupService::TooManyCustomersError
-    too_many_customers_message(given_name)
+    too_many_customers_message
   rescue CustomerLookupService::MoreThanOneMatchingCustomerError
-    more_than_one_matching_customer_message(given_name)
+    more_than_one_matching_customer_message
   rescue
-    generic_error(given_name)
+    generic_error
+  end
+
+  def given_name
+    request.intent.slots['GivenName']
+  end
+
+  def amount
+    request.intent.slots['Amount'].to_i
+  end
+
+  def amount_in_pence
+    amount * 100
   end
 
   def customers
@@ -40,27 +46,27 @@ class ChargeCustomerHandler < AlexaSkillsRuby::Handler
     session.attributes['access_token'] || Prius.get(:gocardless_access_token)
   end
 
-  def charged_successfully_message(given_name, amount, charge_date)
+  def charged_successfully_message(charge_date)
     response.set_output_speech_text("We've successfully charged #{given_name} #{amount}" \
                                     "pounds, and they'll be charged on #{charge_date}.")
   end
 
-  def too_many_customers_message(given_name)
+  def too_many_customers_message
     response.set_output_speech_text('Sorry - you have too many Go Cardless customers, ' \
                                     "so we couldn't look up #{given_name}.")
   end
 
-  def more_than_one_matching_customer_message(given_name)
+  def more_than_one_matching_customer_message
     response.set_output_speech_text('Sorry - we found more than one customer called ' \
                                     "#{given_name}, so we couldn't tell who to charge.")
   end
 
-  def customer_not_found_message(given_name)
+  def customer_not_found_message
     response.set_output_speech_text("Sorry - we couldn't find a customer called " \
                                     "#{given_name} - check your Dashboard and try again.")
   end
 
-  def generic_error(given_name)
+  def generic_error
     response.set_output_speech_text('Sorry - something went wrong when we were charging' \
                                     " #{given_name}. Check your Dashboard to make sure " \
                                     "they weren't charged, then try again.")
